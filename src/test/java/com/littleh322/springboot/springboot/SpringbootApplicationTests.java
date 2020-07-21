670dd9678cbd7a6f1728286300d54ac1d6031948
Joseph Haberberger <joe.haberberger@qflow.com>
2020-07-20 20:29:55 -0600

    setting up springboot with gradle.

---
+++ src/test/java/com/littleh322/springboot/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littleh322.springboot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
