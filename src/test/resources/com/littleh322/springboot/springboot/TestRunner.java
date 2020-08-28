39fa50a35b942c470bd716edd119d07eb9cfd90d
Joseph Haberberger <joe.haberberger@qflow.com>
2020-08-28 07:43:13 -0600

    cleanup

--- src/test/resources/com/littleh322/springboot/springboot/TestRunner.java
+++ src/test/resources/com/littleh322/springboot/springboot/TestRunner.java
@@ -4,11 +4,11 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-import com.littleh322.springboot.springboot.tests.ParameterizedTestWithJSON;
+import com.littleh322.springboot.springboot.tests.ParameterizedEmployees;
 
 public class TestRunner {
 	public static void main(String[] args) {
-		Result result = JUnitCore.runClasses(ParameterizedTestWithJSON.class);
+		Result result = JUnitCore.runClasses(ParameterizedEmployees.class);
 
 		for (Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
